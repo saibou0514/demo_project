d7216ddbbb91d6d09c7916ca0efbe4ecef1dd3fb
saibo <saibo@LAPTOP-300T6GLU>
2022-10-06 11:00:21 +0800

    first commit

---
+++ src/test/java/com/example/demo1006/Demo1006ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo1006;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1006ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
